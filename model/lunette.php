d290e22e6a19f99b387aa449f186a5151fa4145d
PRAPANT Barbara <prapantb@hostname.iut2.upmf-grenoble.fr>
2018-10-18 08:43:38 +0200

    Creation de lunette objet

---
+++ model/lunette.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Un article en vente
+    class Lunette {
+        public $intitulé;// Nom de l'article
+        public $compleinfo;//Complépments d'info
+        public $prix;// le prix
+        public $reference;// la référence unique
+        public $categorie;// identifiant de catégorie
+        public $photo// Nom du fichier image
+    }
+
+?>
