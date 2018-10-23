28421c3bd5932033a6d452b9915ecea334e71c0a
GIROUD Thibaut <giroudth@hostname.iut2.upmf-grenoble.fr>
2018-10-23 08:52:51 +0200

    ajout login admin

--- view/login-admin.view.php
+++ view/login-admin.view.php
@@ -5,6 +5,14 @@
     <title></title>
   </head>
   <body>
-
+    <form method="post" action="../controler/login.ctrl.php">
+      <p>
+        <label for="pseudo">Votre pseudo</label> : <input type="text" name="pseudo" id="pseudo"/>
+        <label for="motdepasse">Mot de passe</label> : <input type="password" name="motdepasse" id="motdepasse"/>
+      </p>
+      <p>
+        <input type="submit" value="Envoyer" />
+      </p>
+    </form>
   </body>
 </html>
