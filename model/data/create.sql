6a31ba65935c7c24cad0a8fe8b2cde7f986be7fa
Barbara <bab38prapant@hotmail.fr>
2018-10-24 19:27:45 +0200

    update catégorie

--- model/data/create.sql
+++ model/data/create.sql
@@ -1,20 +1,24 @@
-CREATE TABLE lunette (
-Numero INTEGER,
-Marque STRING,
-Modèle STRING,
-Genre CHARACTER,
-Style STRING,
-Forme STRING,
-Couleur STRING,
-Matériaux STRING,
-Prix INTEGER
+CREATE TABLE lunette
+(
+  Numero INTEGER,
+  Marque STRING,
+  Modèle STRING,
+  Genre CHARACTER,
+  Style STRING,
+  Forme STRING,
+  Couleur STRING,
+  Matériaux STRING,
+  Prix INTEGER
 );
 
-CREATE TABLE categorie{
-  
-}
+CREATE TABLE categorie
+(
+  Id STRING,
+  Nom STRING
+);
 
-CREATE TABLE login (
+CREATE TABLE login
+(
   login STRING,
   motdp STRING
 );
