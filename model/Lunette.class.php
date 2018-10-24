32a62f0df7ae5c8ce5777ed442ba6d473fdb13bf
PRAPANT Barbara <prapantb@hostname.iut2.upmf-grenoble.fr>
2018-10-24 19:05:03 +0200

    updtae lunette.class

--- model/Lunette.class.php
+++ model/Lunette.class.php
@@ -2,12 +2,16 @@
 
     // Une lunette
     class Lunette {
-        public $intitulé;// Nom de l'article
-        public $compleinfo;//Complépments d'info
-        public $prix;// le prix
-        public $reference;// la référence unique
-        public $categorie;// identifiant de catégorie
-        public $photo// Nom du fichier image
+        public $numero;
+        public $marque;
+        public $modele;
+        public $genre;
+        public $style;
+        public $forme;
+        public $couleur;
+        public $materiau;
+        public $prix;
     }
+
 
 ?>
