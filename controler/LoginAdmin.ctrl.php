224ee5b14c4e356c6a219e6f00998078b2ca053a
PRAPANT Barbara <prapantb@hostname.iut2.upmf-grenoble.fr>
2018-11-08 15:37:43 +0100

    ajout login

--- controler/LoginAdmin.ctrl.php
+++ controler/LoginAdmin.ctrl.php
@@ -1,7 +1,15 @@
 <?php
-  $pseudo = $_Post['pseudo'];
+  require_once("../model/DAO.class.php");
+  require_once("../model/LoginAdmin.class.php");
+
+  $pseudo = $_POST['pseudo'];
   $pass = $_POST['pass'];
+  $bd = new DAO();
 
+  $login = $bd->getlogin();
+  $list = $bd->getAllLunettes();
+  if ($pseudo==$login->login && $pass==$login->motdp) {
+    include("../view/interfaceAdmin.view.php");
+  }
 
-  include("../view/LoginAdmin.view.php");
 ?>
